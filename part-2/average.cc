bb995cd437644cd01e1d13c1a523bdecb2f75a1e
Ethan Ramos <ethan52146458@csu.fullerton.edu>
2023-10-24 20:43:12 -0700

    First commit

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Ethan Ramos
+// ethan52146458@csu.fullerton.edu
+// @RahmMane13
+// Partners: @lemusdan
 
 #include <iostream>
 #include <string>
@@ -7,10 +10,14 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that there is at least one command line argument.
+  // XTODO: Validate that there is at least one command line argument.
   // If not, print an error message and return a non-zero value.
+  if (arguments.size() <= 1) {
+    std::cout << "error: you must supply at least one number";
+    return 1;
+  }
 
-  // TODO: Write a for-each loop to sum (add up) all of the command line
+  // XTODO: Write a for-each loop to sum (add up) all of the command line
   // arguments.
   // Use a double or float type so that your program preserves fractional
   // values.
@@ -18,14 +25,25 @@ int main(int argc, char* argv[]) {
   // of the arguments vector.
   // Each argument is a std::string. You will need to convert each string into
   // a number with the std::stod or std::stof function.
-
-  // TODO: After the loop has finished summing the arguments, calculate the
+  double total{0.0};
+  bool first{true};
+  for (std::string& arg : arguments) {
+    if (first) {
+      (first) = false;
+    } else {
+      double number{std::stod(arg)};
+      total += number;
+    }
+  }
+  // XTODO: After the loop has finished summing the arguments, calculate the
   // average of the values. Recall that the average is the total value divided
   // by the number of values.
+  double average{0};
+  average = (total / (static_cast<int>(arguments.size()) - 1));
 
-  // TODO: Use cout to print out a message of the form
+  // XTODO: Use cout to print out a message of the form
   // average = *AVERAGE*
   // on its own line.
-
+  std::cout << "average = " << average;
   return 0;
 }
